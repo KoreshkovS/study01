bacebacbe9c3179c992925091e4189c9f1eb7524
KoreshkovS <110051047+KoreshkovS@users.noreply.github.com>
2023-10-11 10:01:39 +0300

    followScript

---
+++ Assets/Scripts/SimpleExample.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleExample : MonoBehaviour
+{
+    [SerializeField] private Transform _objToFolow;
+
+    private Vector3 _deltaPos;
+
+    void Start()
+    {
+        _deltaPos = transform.position - _objToFolow.position;
+    }
+
+    void Update()
+    {
+        transform.position = _objToFolow.position + _deltaPos;
+    }
+}
