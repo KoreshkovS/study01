93de7d81701c854c93c22382af99f020b5483bed
KoreshkovS <110051047+KoreshkovS@users.noreply.github.com>
2023-10-12 22:17:51 +0300

    added music

---
+++ Assets/Scripts/PlayerAudioTest.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudioTest : MonoBehaviour
+{
+    [SerializeField] private AudioSource _collisionSound;   
+}
